0e6b02347406d7496363baccc3d6c6ad8b86cf9f
anurag <anuragv.1020@gmail.com>
2022-08-04 00:54:47 +0530

    added sign in with redirect feature.. for now its commented but can be enabled by just uncommenting it under sign-in component

--- src/routes/signIn/sign-in.component.jsx
+++ src/routes/signIn/sign-in.component.jsx
@@ -1,15 +1,46 @@
-import { signInWithGooglePopup } from '../../utils/firebase/firebase.util';
+import { useEffect } from 'react';
+import { getRedirectResult } from 'firebase/auth';
+import {
+    auth, // for redirect login
+    signInWithGooglePopup, 
+    signInWithGoogleRedirect 
+} from '../../utils/firebase/firebase.util';
 import { createuserDocumentFromAuth } from '../../utils/firebase/firebase.util';
 const SignIn = ()=>{
+    /*
+        uncomment it if u want to use login with redirect
+        useEffect(()=>{
+            (async function(){
+                const response = await getRedirectResult(auth);
+                if(response){
+                    const userDocRef = await createuserDocumentFromAuth(response.user);
+                    console.log(userDocRef)
+                }
+            })();
+        },[]); // on component mount/unmount
+    */
+
     const logGoogleUser = async ()=>{
         const {user} = await signInWithGooglePopup();
         // console.log(response);
         const userDocRef = await createuserDocumentFromAuth(user);
+        console.log(userDocRef)
     }
+    /*
+        below method will not work as after being redirect all the components will unmount and we'll lost all the states
+        instead use effects
+        const logGoogleUserRedirect = async()=>{ 
+            const {user} = await signInWithGoogleRedirect();
+            // console.log(response);
+            const userDocRef = await createuserDocumentFromAuth(user);
+            console.log(userDocRef)
+        }
+    */
     return (
         <div>
             <h1>Sign In Page!</h1>
             <button onClick={logGoogleUser}>Sign In with Google</button>
+            {/* <button onClick={signInWithGoogleRedirect}>Sign In with Google Redirect</button> */}
         </div>
     )
 }
--- src/utils/firebase/firebase.util.js
+++ src/utils/firebase/firebase.util.js
@@ -34,16 +34,18 @@ provider.setCustomParameters({
 
 export const auth = getAuth();
 export const signInWithGooglePopup = ()=> signInWithPopup(auth, provider);
+export const signInWithGoogleRedirect = ()=> signInWithRedirect(auth, provider);
 
 export const db = getFirestore();
+
 export const createuserDocumentFromAuth = async (userAuth)=>{
     const userDocRef = doc(db, 'users', userAuth.uid);
-    console.log(userDocRef)
+    // console.log(userDocRef)
     /*
         create a document reference regardless to the document whether it exists or not
     */
    const userSnapshot = await getDoc(userDocRef);
-   console.log(userSnapshot.exists())
+//    console.log(userSnapshot.exists())
 
    /*
         if user data exists
